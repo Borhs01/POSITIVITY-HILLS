c12e049ac5518957fe260bec77e61fd0e3d47abb
Borhs01 <155558801+Borhs01@users.noreply.github.com>
2025-06-18 19:38:13 +0100

    POSITIVE HILLS

---
+++ index.js
@@ -0,0 +1,30 @@
+document.querySelectorAll('.section__faqs__right--examples')[0].addEventListener('click', function() {
+  const faqsText = document.querySelectorAll('.faqs__text')[0];
+  faqsText.style.display = faqsText.style.display === 'block' ? 'none' : 'block';
+});
+document.querySelectorAll('.section__faqs__right--examples')[1].addEventListener('click', function() {
+  const faqsText = document.querySelectorAll('.faqs__text')[1];
+  faqsText.style.display = faqsText.style.display === 'block' ? 'none' : 'block';
+});
+document.querySelectorAll('.section__faqs__right--examples')[2].addEventListener('click', function() {
+  const faqsText = document.querySelectorAll('.faqs__text')[2];
+  faqsText.style.display = faqsText.style.display === 'block' ? 'none' : 'block';
+});
+
+document.querySelectorAll('.section__faqs__right--examples')[3].addEventListener('click', function() {
+  const faqsText = document.querySelectorAll('.faqs__text')[3];
+  faqsText.style.display = faqsText.style.display === 'block' ? 'none' : 'block';
+});
+
+document.querySelectorAll('.section__faqs__right--examples')[4].addEventListener('click', function() {
+  const faqsText = document.querySelectorAll('.faqs__text')[4];
+  faqsText.style.display = faqsText.style.display === 'block' ? 'none' : 'block';
+});
+document.querySelectorAll('.section__faqs__right--examples')[5].addEventListener('click', function() {
+  const faqsText = document.querySelectorAll('.faqs__text')[5];
+  faqsText.style.display = faqsText.style.display === 'block' ? 'none' : 'block';
+});
+
+
+
+
